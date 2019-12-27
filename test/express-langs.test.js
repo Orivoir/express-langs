ed73640ee53cf59af8e63daee007ec1be181194f
S.Gaborieau <sam.gabor@hotmail.com>
2019-12-27 14:52:02 +0100

    optimize test unit

--- test/express-langs.test.js
+++ test/express-langs.test.js
@@ -5,63 +5,97 @@ const
 
 describe('middleware' , () => {
 
-    describe('request handler create' , () => {
+    let reqFactory = {
+        headers: {
+            "accept-language": "test,test-TEST;q=0.8,TEST-2;q=0.5,en;q=0.3"
+        }
+    } ;
 
-        let mw = null ;
+    const mw = middleware( reqFactory , null , () => {
+        console.log('\n\n\t-- free middleware --\n');
+    } ) ;
 
-        let reqFactory = {
-            headers: {
-                "accept-language": "test,test-TEST;q=0.8,TEST-2;q=0.5,en;q=0.3"
-            }
-        } ;
 
-        mw = middleware( reqFactory , null , () => {
-            console.log('\n\n\t-- free middleware --\n');
-        } ) ;
+    describe('middleware entry point test' , () => {
+
 
         it('should be an function' , () => {
 
             assert.isFunction( middleware , 'mw() is an function' ) ;
         } ) ;
 
+    } ) ;
 
-        describe('test parse langs' , () => {
+    describe('test parse langs' , () => {
 
-            it('`request.langs` should be an object' , () => {
+        it('`request.langs` should be an object' , () => {
 
-                assert.isObject( reqFactory.langs , 'langs is object' ) ;
-
-            } ) ;
-
-            it('should have an attribute items array[object] ' , () => {
-
-                expect( reqFactory.langs ).to.have.property( 'items' ) ;
-
-                expect( reqFactory.langs.items ).to.be.an( 'array' ) ;
-
-            } ) ;
-
-            it('should have 3 langs founds' , () => {
-
-                expect( reqFactory.langs.items ).lengthOf( 3 ) ;
-
-            } ) ;
-
-            describe('should have exists methods' , () => {
-
-                it('should have two exists Functions' , () => {
-
-                    assert.isFunction( reqFactory.langs.exists , 'exists is () => boolean' ) ;
-                    assert.isFunction( reqFactory.langs.existsStrict , 'existsStrict is () => boolean' ) ;
-
-                } ) ;
-
-
-
-            } ) ;
+            assert.isObject( reqFactory.langs , 'langs is object' ) ;
 
         } ) ;
 
+        it('should have an attribute items array[object] ' , () => {
+
+            expect( reqFactory.langs ).to.have.property( 'items' ) ;
+
+            expect( reqFactory.langs.items ).to.be.an( 'array' ) ;
+
+        } ) ;
+
+        it('should have 3 langs founds' , () => {
+
+            expect( reqFactory.langs.items ).lengthOf( 3 ) ;
+
+        } ) ;
+
+    } ) ;
+
+    describe('exists methods parse' , () => {
+
+        it('should have two exists Functions' , () => {
+
+            assert.isFunction( reqFactory.langs.exists , 'exists is () => boolean' ) ;
+            assert.isFunction( reqFactory.langs.existsStrict , 'existsStrict is () => boolean' ) ;
+
+        } ) ;
+
+        it('should be throw TypeError' , () => {
+
+            try {
+                expect( reqFactory.langs.exists( 42 ) ).to.throw( TypeError ) ;
+            } catch( TypeError ) {
+                console.log('have success throw TypeError');
+            }
+
+            try {
+                expect( reqFactory.langs.existsStrict( 42 ) ).to.throw( TypeError ) ;
+            } catch( TypeError ) {
+                console.log('have success throw TypeError');
+            }
+
+
+        } ) ;
+
+
+    } ) ;
+
+    describe('build regexp () => object' , () => {
+
+        const buildRegexpTest = require('./../lib/build-regexp') ;
+
+        const regBuild = buildRegexpTest('correct\-format' , "i" ) ;
+
+        it('should be an Function' , () => {
+
+            assert.isFunction( buildRegexpTest ) ;
+
+        } ) ;
+
+        it('should be have source property' , () => {
+
+            expect( regBuild ).to.have.property( 'source' ) ;
+
+        } ) ;
 
     } ) ;
 
